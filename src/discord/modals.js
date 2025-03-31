7f6826545e98b6cc92b165d9a3bac8b57701cdb9
RedLime <redlimerl@gmail.com>
2025-04-01 02:55:34 +0900

    Fix setup buttons permission check

--- src/discord/modals.js
+++ src/discord/modals.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageFlags, ModalSubmitInteraction } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, EmbedBuilder, MessageFlags, ModalSubmitInteraction, PermissionsBitField } from "discord.js";
 import Submission from "../class/submission.js";
 import LevelRequest from "../class/request.js";
 import ServerManager from "../class/server_manager.js";
@@ -63,11 +63,22 @@ export default [
     {
         id: 'setup',
         async execute(interaction) {
-            await interaction.deferUpdate();
+            await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
             const context = interaction.fields.getTextInputValue('context') || null;
             const embeds = [];
             if (context?.trim()) {
                 embeds.push(new EmbedBuilder().setDescription(context));
+            }
+            if (!interaction.channel || interaction.channel.type != ChannelType.GuildText) {
+                await interaction.editReply({ content: app.message.errorReviewUpdate + '\n' + app.messageFormat('errorChannelInvalid', interaction.channel.id), embeds: [], components: [], flags: MessageFlags.Ephemeral });
+                return;
+            }
+
+            const permission = interaction.channel.permissionsFor(interaction.guild.members.me);
+            if (!permission.has([PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages])) {
+                await interaction.editReply({ content: app.message.errorReviewUpdate + '\n' + app.messageFormat('errorPermissionChannelMessage', interaction.channel.id), embeds: [], components: [], flags: MessageFlags.Ephemeral });
+                return null;
             }
             await interaction.channel.send({ embeds, components: [new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId('submit').setEmoji(app.emoji.sent_normal).setLabel('Submit the Level Request').setStyle(ButtonStyle.Primary))] });
         }
